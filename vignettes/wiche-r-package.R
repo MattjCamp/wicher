3d7314530a49051a47d1a194d2fb0d5e157073d6
mattjcamp <mjcampbell74@gmail.com>
2016-10-06 10:17:18 -0400

    updated vignette

--- vignettes/wiche-r-package.R
+++ vignettes/wiche-r-package.R
@@ -1,34 +1,69 @@
 ## ----warning = FALSE, message = FALSE, echo = TRUE-----------------------
 
-enrollments <- lookup_wiche(year = 2012:2016, gender = c("f", "m"))
+library(wicher)
+
+head(wiche_2013)
 
 
 ## ----warning = FALSE, message = FALSE, echo = TRUE-----------------------
 
+lookup_wiche(year = 2012:2016,
+             gender = c("f", "m"))
+
+
+## ----warning = FALSE, message = FALSE, echo = TRUE-----------------------
+
+lookup_wiche(location = c("pa","ny"),
+             year = 2012:2016,
+             gender = c("f", "m"))
+
+
+## ----fig.width = 7, fig.height = 4, echo = TRUE--------------------------
+
 library(ggplot2)
 
-enrollments <- lookup_wiche(year = 2006:2026, gender = c("f", "m"))
+enrollments <- lookup_wiche(year = 2000:2010,
+                            location = "ca",
+                            race = c("white", "hispanic"))
 
 qplot(year, n, data = enrollments, 
-      color = gender, 
+      color = race, 
       geom = c("point", "smooth"),
-      main = "WICHE Projections",
+      main = "California Enrollment",
       xlab = "Academic Year", 
-      ylab = "Projection")
+      ylab = "Enrollment")
 
 
-## ------------------------------------------------------------------------
+## ----fig.width = 7, fig.height = 4, echo = TRUE--------------------------
 
-wiche_us <- lookup_wiche(year = 2006, gender = c("f", "m"))
-ggplot(data = wiche_us, aes(gender, n)) + 
-  geom_bar(stat = "identity", position = "dodge")
+ggplot(enrollments, aes(x = as.character(year), y = n, group = race)) +
+  geom_smooth(aes(color = race)) +
+  geom_point(aes(color = race)) + 
+  ggtitle("California Enrollment") +
+  labs(x = "Academic Year", y = "Enrollment")
 
-wiche_us <- lookup_wiche(year = 2006,
-                         race = c("white", "asian", "hispanic", "native", "black"))
-ggplot(data = wiche_us, aes(race, n)) + 
+
+## ----fig.width = 7, fig.height = 4, echo = TRUE--------------------------
+
+enrollments <- lookup_wiche(year = 2000,
+                            location = "ca",
+                            race = c("white", "asian", "hispanic", "native", "black"))
+
+ggplot(data = enrollments, aes(race, n)) + 
   geom_bar(stat = "identity", position = "dodge") + 
-  xlab("Race/Ethnicty") + 
-  ylab("Projection") +
-  ggtitle("Wiche Projections")
+  ggtitle("California Enrollment (2000)") +
+  labs(x = "Academic Year", y = "Enrollment")
+
+
+## ----fig.width = 7, fig.height = 4, echo = TRUE--------------------------
+
+enrollments <- lookup_wiche(year = 2010,
+                            location = "ca",
+                            race = c("white", "asian", "hispanic", "native", "black"))
+
+ggplot(data = enrollments, aes(race, n)) + 
+  geom_bar(stat = "identity", position = "dodge") + 
+  ggtitle("California Enrollment (2010)") +
+  labs(x = "Academic Year", y = "Enrollment")
 
 
