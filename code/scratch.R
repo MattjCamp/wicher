4cad51907f601676617621c04e485f0dd4447330
Matthew Campbell <mjcampbell74@gmail.com>
2017-04-21 14:54:26 -0400

    Fixed issue where 0 should have been NA

--- code/scratch.R
+++ code/scratch.R
@@ -1,7 +1,57 @@
+# CHECK NULL
+
+wb %>% filter(STABR == "WI",
+              SURVYEAR == 2028,
+              TYPE == "G") %>%
+  as.data.frame()
+
+w1 %>% filter(location == "wi",
+              year == 2029,
+              grade %in% c("8","9"),
+              type == "p") %>%
+  as.data.frame()
+
+w2 %>% filter(location == "wi",
+              year == 2029,
+              grade %in% c("8","9"),
+              type == "p") %>%
+  as.data.frame()
+
+w3 %>% filter(location == "wi",
+              year == 2029,
+              grade %in% c("8","9"),
+              type == "p") %>%
+  as.data.frame()
+
+w4 %>% filter(location == "wi",
+              year == 2029,
+              grade %in% c("8","9"),
+              race == "all",
+              sector == "p") %>%
+  as.data.frame()
+
+wicher::wiche_graduate_projections %>%
+  filter(location == "wi",
+              year == 2029,
+              grade %in% c("8","9"),
+              race == "all",
+              sector == "p") %>%
+  as.data.frame()
+
+wicher::wiche_graduate_projections %>%
+  filter(location == "al",
+              year == 2025,
+              grade %in% c("1"),
+              race == "all",
+              sector == "p") %>%
+  as.data.frame()
+
 
 # WI
 
-wb %>% filter(STABR == "WI", SURVYEAR == 2003, TYPE == "G") %>%
+wb %>% filter(STABR == "WI",
+              SURVYEAR == 2003,
+              TYPE == "G") %>%
   as.data.frame()
 
 w1 %>% filter(location == "wi", year == 2004,
@@ -42,5 +92,19 @@ w1 %>% filter(location == "wi", year %in% c(2013,2014,2015),
 
 w1 %>% filter(location == "wi", year %in% c(2010),
               grade == "g", type == "np") %>%
+  as.data.frame()
+
+# CHECK LATER YEARS THAT SHOULD BE NULL
+
+w1 %>% filter(location == "al",
+              year %in% c(2025),
+              grade == "1",
+              type %in% c("all","p","np")) %>%
+  as.data.frame()
+
+w4 %>% filter(location == "al",
+              year %in% c(2025),
+              grade == "1",
+              sector %in% c("all","p","np")) %>%
   as.data.frame()
 
