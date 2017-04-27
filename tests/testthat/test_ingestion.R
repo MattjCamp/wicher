b4d5847559aab237b09d5315445d3843c8a136a2
Matthew Campbell <mjcampbell74@gmail.com>
2017-04-27 14:18:26 -0400

    Function working, but not getting put into package?

--- tests/testthat/test_ingestion.R
+++ tests/testthat/test_ingestion.R
@@ -2,15 +2,42 @@
 library(tidyverse)
 library(dbr)
 library(testthat)
+library(wicher)
 
 context("DATA INGESTION")
 
 test_that("ROUNDING - DATAFRAME", {
 
-  w <- wicher::wiche_ingest_enrollments("./code/All-Enrollment-and-Graduate-Projections-neh9.xlsx")
+  # Rounding issues when working from the downloadable Excel file from the WICHE website.
+  #   - Some cells (but not many) in the WICHE downloadable file are decimal values. It looks like in these
+  #   - This difference is likely because the Wisconsin public value in the Excel downloadable file is
+  #   63,250.5. Perhaps, this is getting truncated to 63,250 in your file instead of 63,251?
 
-  d <- w %>% filter(location == "wi", year == 2004,
-                    sector == "p", grade == "g",
+
+  # # DEBUGGING
+  #
+  # # RAW
+  #
+  # wb %>%
+  #   filter(SURVYEAR == 2003,
+  #          STABR == "WI",
+  #          TYPE == "G") %>%
+  #   select(STABR, SURVYEAR, TYPE, DPL) %>%
+  #   as.data.frame()
+  #
+  # # STEP ONE
+  #
+  # w1 %>%
+  #   filter(location == "wi",
+  #          year == 2004,
+  #          grade == "g")
+
+  w <- wiche_ingest_enrollments("./code/All-Enrollment-and-Graduate-Projections-neh9.xlsx")
+
+  d <- w %>% filter(location == "wi",
+                    year == 2004,
+                    sector == "p",
+                    grade == "g",
                     race == "all")
 
   expect_equal(d$n, 63251)
